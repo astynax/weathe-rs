14d010b252ccb910695142a892184df24c54768d
Aleksey Pirogov <astynax84@gmail.com>
2015-07-22 16:06:45 +0300

    OpenWeatherMap provider now is fully functional

--- src/providers/owm/mod.rs
+++ src/providers/owm/mod.rs
@@ -1,26 +1,49 @@
 //! # Weather provider using *OpenWeatherMaps*
 
-use std::io::Read;
+use std::borrow::Borrow;
 
-extern crate serde;
+extern crate rustc_serialize;
 
-use self::serde::json;
+use self::rustc_serialize::json::Json;
 
 use types::{WeatherInfo, WeatherResult, TempUnit};
 use providers::request;
 
+
+fn parse_weather(raw: String, units: TempUnit) -> WeatherResult {
+    Json::from_str(raw.borrow()).ok().and_then(
+        |j| {
+            let temperature = j.find_path(&["main", "temp"]).and_then(
+                |t| t.as_f64());
+            j.as_object().and_then(
+                |o| {
+                    let description = o.get("weather").and_then(
+                        |j| j.as_array().and_then(
+                            |a| a[0].as_object().and_then(
+                                |o| o.get("description").and_then(
+                                    |d| d.as_string()))));
+
+                    description.and_then(
+                        |d| temperature.and_then(
+                            |t| Some(WeatherInfo::new(
+                                d.to_string(), t as i8, units))))
+                })
+        })
+        .ok_or("Foo!".to_string())
+}
+
+
 pub fn owm_weather(city: String, units: TempUnit) -> WeatherResult {
     let mut content = String::new();
     if request(
-        "http://api.openweathermap.org/data/2.5/weather".to_string()
-            + "?id=" + &city
+        "http://api.openweathermap.org/data/2.5/weather?lang=ru".to_string()
+            + "&id=" + &city
             + "&units=" + match units {
                 TempUnit::Celsius => "metric",
                 TempUnit::Fahrenheit => "imperial"
             },
         &mut content) {
-        println!("{}", content);
-        Err("Baz!".to_string())
+        parse_weather(content, units)
     } else {
         Err("Foo!".to_string())
     }
